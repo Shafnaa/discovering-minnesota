4d0f9f88e6493bdd1856b083e143b9bddb66f5dc
Shafnaa <saujanashafi2003@gmail.com>
2023-09-08 09:16:59 +0700

    first commit

---
+++ assets/script/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#131824",
+        gray: "#2B2F3A",
+        lakes: "#35CFE8",
+        resorts: "#EDAE4B",
+        parks: "#D82848",
+        hiking: "#1CA954",
+        fishing: "#4B96DB",
+        skiing: "#7E53B4",
+      },
+    },
+  },
+};
